7f928ebfe590eef07111dc30b0be0a342bad0ebf
dgrigoriadis@infili.com <Grigoriadis!>
2022-07-12 20:40:58 +0300

    initial commit

--- src/main/java/net/grigdim/sms/entities/Student.java
+++ src/main/java/net/grigdim/sms/entities/Student.java
@@ -1,6 +1,9 @@
 package net.grigdim.sms.entities;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "students")
@@ -10,10 +13,13 @@ public class Student {
     @Column(name = "id")
     private Long id;
     @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "First name cannot be null")
     private String firstName;
     @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Last name cannot be null")
     private String lastName;
     @Column(name = "email")
+    @Email
     private String email;
 
     public Student() {
